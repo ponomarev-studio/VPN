4a2a229841d2a2fce4a50ccf121bd1dcc6ba74f3
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2026-04-09 01:16:41 +0200

    Update default server port from 8080 to 3000 in `start.sh`

--- start.sh
+++ start.sh
@@ -13,8 +13,8 @@ ip6tables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 tailscaled --state=/data/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock --socks5-server=:1080 --outbound-http-proxy-listen=:1080 &
 tailscale up --auth-key=${TS_AUTHKEY} --hostname=${FLY_REGION:-vpn} --advertise-exit-node --ssh --reset
 
-tailscale funnel --bg 8080
+tailscale funnel --bg 3000
 
 TS_DOMAIN="${FLY_REGION:-vpn}.${TS_TAILNET}"
 
-exec proxyt serve --http-only --port 8080 --domain "${PROXYT_DOMAIN:-${TS_DOMAIN}}"
+exec proxyt serve --http-only --port 3000 --domain "${PROXYT_DOMAIN:-${TS_DOMAIN}}"
