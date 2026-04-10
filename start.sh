d741cd6de4d66787dc6b2b10c321e6bbc39ab11a
Vladislav Ponomarev <2877584+PonomareVlad@users.noreply.github.com>
2026-04-10 15:51:45 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-modprobe xt_mark
+# modprobe xt_mark
 
 echo 'net.ipv4.ip_forward = 1' | tee -a /etc/sysctl.conf
 echo 'net.ipv6.conf.all.forwarding = 1' | tee -a /etc/sysctl.conf
