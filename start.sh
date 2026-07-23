536af7d111abb99720b3c48fc1a59f994ffe1267
Vladislav Ponomarev <2877584+PonomareVlad@users.noreply.github.com>
2026-07-23 15:53:53 +0200

    Refactor start.sh to use proxyt for serving
    Updated start.sh to replace tailscaled and tailscale commands with proxyt serve and funnel commands.

--- start.sh
+++ start.sh
@@ -1,21 +1,24 @@
 #!/bin/sh
 set -e
 
-# modprobe xt_mark
+tailscaled \
+  --state=/data/tailscale/tailscaled.state \
+  --socket=/var/run/tailscale/tailscaled.sock \
+  --tun=userspace-networking &
 
-echo 'net.ipv4.ip_forward = 1' | tee -a /etc/sysctl.conf
-echo 'net.ipv6.conf.all.forwarding = 1' | tee -a /etc/sysctl.conf
-sysctl -p /etc/sysctl.conf
+sleep 2
 
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-ip6tables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+tailscale up \
+  --auth-key="$TS_AUTHKEY" \
+  --hostname="$TS_HOSTNAME"
 
-tailscaled --state=/data/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock --tun=userspace-networking --socks5-server=:1080 --outbound-http-proxy-listen=:1080 &
-tailscale up --auth-key=${TS_AUTHKEY} --hostname=${TS_HOSTNAME:-cloud-ru} --advertise-exit-node --ssh --reset
-tailscale web --readonly --listen 0.0.0.0:8080 &
+proxyt serve \
+  --http-only \
+  --port 3000 \
+  --domain "${PROXYT_DOMAIN:-${TS_HOSTNAME}.${TS_TAILNET}}" &
+
+sleep 1
 
 tailscale funnel --bg 3000
 
-TS_DOMAIN="${TS_HOSTNAME:-cloud-ru}.${TS_TAILNET}"
-
-exec proxyt serve --http-only --port 3000 --domain "${PROXYT_DOMAIN:-${TS_DOMAIN}}"
+wait
